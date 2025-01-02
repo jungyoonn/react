3c79f38416438463a2913e5e8dfc689f2cfdfcd7
jungyoonn <sophia7625@naver.com>
2025-01-02 18:19:02 +0900

    렌더링

---
+++ src/component/MyElement.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName : 'jungyoon',
+  lastName : 'heo'
+}
+
+const comment = "인사말";
+
+// title : 커서 올렸을 때 '' 안의 내용이 뜸
+const el = <h1 title={comment}>
+hello {formatName(user)}
+</h1>;
+
+const MyElement = () => {
+  return el;
+}
+
+export default MyElement;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MyElement from './component/MyElement';
+
+import Library from './chapter_03/Library';
+import Library2 from './chapter_03/Library2';
+import Clock from './chapter_04/Clock';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+setInterval(() => {
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    {/* <MyElement /> */}
+    {/* <Library2 /> */}
+    <Clock />
   </React.StrictMode>
-);
+  );
+}, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
