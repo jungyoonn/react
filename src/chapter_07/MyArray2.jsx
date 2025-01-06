64d5143803d96df70e7d8d346f04b4aa08811245
jungyoonn <sophia7625@naver.com>
2025-01-06 09:49:36 +0900

    과일 갯수를 랜덤으로 추가하는 예제

--- src/chapter_07/MyArray2.jsx
+++ src/chapter_07/MyArray2.jsx
@@ -3,27 +3,24 @@ import { useState } from 'react';
 
 const MyArray2 = () => {
   const fruits = ['사과', '딸기', '포도', '복숭아', '귤'];
-  const [bucket, setBucket] = useState([]);
-  const [count, setCount] = useState([0, 0, 0, 0, 0]);
+  // 초기값
+  const counts = fruits.map(f => ({name: f, count: 0}));
+  // 초기값의 복제본을 쓴다
+  const [bucket, setBucket] = useState([...counts]);
   return (
     <div>
-      <h3>{bucket}</h3>
+      {/* <h3>{bucket}</h3> */}
       <button onClick={() => {
-        const rand = (Math.floor)(Math.random() * fruits.length);
-        count[rand] += 1;
-        setCount(count[rand]);
+        const rand = parseInt(Math.random() * bucket.length);
+        bucket[rand].count++; // bucket 자체는 변하지 않기 때문에 불변성을 만족함
+        setBucket([...bucket]);
         }}>과일 추가</button>
       <button onClick={() => {
-        const rand = (Math.floor)(Math.random() * fruits.length);
-        if(count[rand] > 0){
-          setCount(count[rand] - 1);
-        } else {
-          console.log("0개에서 제거할 수 없습니다")
-        }
+        
       }}>과일 빼기</button>
-      <button onClick={() => setBucket([])}>바구니 비우기</button>
+      <button onClick={() => setBucket([...counts])}>바구니 비우기</button>
       <ul>
-        {fruits.map((f, i) => <li key={i}>{f} {count[i]}개</li>)}
+        {bucket.map((f, i) => <li key={i}>{f.name} {f.count}개</li>)}
       </ul>
     </div>
   );
