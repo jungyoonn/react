49f6a0c0698a682168d1a16da5ddd60d97de081a
jungyoonn <sophia7625@naver.com>
2025-01-08 12:22:23 +0900

    합성

---
+++ src/chapter_13/ProfileCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Card from './Card';
+
+const ProfileCard = (props) => {
+  return (
+    <div>
+      <Card title="eeerrorcode" backgroundColor="lavender">
+        <p>안녕하세요, 허정윤입니다</p>
+        <p>저는 리액트를 사용해서 개발하고 있습니다</p>
+      </Card>
+    </div>
+  );
+}
+
+export default ProfileCard;
